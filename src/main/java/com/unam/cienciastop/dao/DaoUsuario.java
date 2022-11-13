d45c32bb12b6f734bd9b65f4266b258d7ea5b908
JorFer <42790879+jorferg@users.noreply.github.com>
2022-11-12 20:28:58 -0600

    E4_07
    Se agrega la anotacion Param que faltaba en DaoUsuario

--- src/main/java/com/unam/cienciastop/dao/DaoUsuario.java
+++ src/main/java/com/unam/cienciastop/dao/DaoUsuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.unam.cienciastop.entity.Usuario;
 
