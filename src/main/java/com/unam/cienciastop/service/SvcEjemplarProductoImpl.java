0e63f0ced7d1842312d29ba623c7a96278ee53bd
JoseEGJasso <josejasso2000@ciencias.unam.mx>
2022-11-14 16:33:41 -0600

    update: manejo de excepciones de ejemplar producto

--- src/main/java/com/unam/cienciastop/service/SvcEjemplarProductoImpl.java
+++ src/main/java/com/unam/cienciastop/service/SvcEjemplarProductoImpl.java
@@ -1,10 +1,13 @@
 package com.unam.cienciastop.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.unam.cienciastop.dao.DaoEjemplarProducto;
 import com.unam.cienciastop.entity.EjemplarProducto;
+import com.unam.cienciastop.exceptionHandler.ApiException;
 
 @Service
 public class SvcEjemplarProductoImpl implements SvcEjemplarProducto {
@@ -14,6 +17,13 @@ public class SvcEjemplarProductoImpl implements SvcEjemplarProducto {
 
     @Override
     public void crearEjemplar(EjemplarProducto item) {
-        repoEProducto.save(item);
+        try {
+            repoEProducto.save(item);    
+        } catch (DataAccessException e){
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "error en la consulta a la base de datos");
+        } catch (Exception e) {
+            throw new ApiException(HttpStatus.NOT_FOUND, e.getLocalizedMessage());
+        }
+        
     }
 }
