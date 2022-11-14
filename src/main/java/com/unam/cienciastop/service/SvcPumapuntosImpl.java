cc729322ea990bba4d8e5e67a59e453174b33dbd
JoseEGJasso <josejasso2000@ciencias.unam.mx>
2022-11-14 16:11:30 -0600

    update: manejo de excepciones en endpoints de pumapuntos

--- src/main/java/com/unam/cienciastop/service/SvcPumapuntosImpl.java
+++ src/main/java/com/unam/cienciastop/service/SvcPumapuntosImpl.java
@@ -1,10 +1,13 @@
 package com.unam.cienciastop.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.unam.cienciastop.dao.DaoPumapuntos;
 import com.unam.cienciastop.entity.Pumapuntos;
+import com.unam.cienciastop.exceptionHandler.ApiException;
 
 @Service
 public class SvcPumapuntosImpl implements SvcPumapuntos{
@@ -13,7 +16,12 @@ public class SvcPumapuntosImpl implements SvcPumapuntos{
 
     @Override
     public Pumapuntos getPumapuntos(Integer idUsuario) {
-        // TODO Auto-generated method stub
-        return repoPuma.findById(idUsuario).get();
+        try {
+            return repoPuma.findById(idUsuario).get();
+        } catch (DataAccessException e){
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "error en la consulta a la base de datos");
+        } catch (Exception e) {
+            throw new ApiException(HttpStatus.NOT_FOUND, e.getLocalizedMessage());
+        }
     }
 }
